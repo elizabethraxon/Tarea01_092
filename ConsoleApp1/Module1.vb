228a1f1db3de33950738c29070a40ad56cb5d959
Silvia <mousai.elly@gmail.com>
2024-07-28 15:31:49 -0600

    Tarea01

---
+++ ConsoleApp1/Module1.vb
@@ -0,0 +1,16 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim mensaje, nombre As String
+        Console.Clear()
+        Console.Write("Ingrese Nombre y #carnet: ")
+        nombre = Console.ReadLine()
+        Console.Clear()
+        mensaje = "Hola Mundo, !!!! "
+        Console.WriteLine(mensaje + nombre + " 2024 !!!! ")
+        Console.Write("Presione cualquier tecla para salir")
+        Console.ReadKey()
+
+    End Sub
+
+End Module
